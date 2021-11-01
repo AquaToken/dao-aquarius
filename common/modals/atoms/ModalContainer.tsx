94182edda3d367615cd4d9e55067e7da9dae201e
shiny-mountain <shiny-mountain@lobstr.co>
2021-11-01 12:56:30 +0300

    Prevent page scrolling when a modal is open

--- common/modals/atoms/ModalContainer.tsx
+++ common/modals/atoms/ModalContainer.tsx
@@ -8,6 +8,24 @@ const ModalContainer = (): JSX.Element => {
     useEffect(() => {
         const unsub = ModalService.event.sub((modals) => {
             setModals(modals);
+
+            const scrollable = document.getElementById('scrollable');
+
+            if (modals.length) {
+                const { y } = scrollable.getBoundingClientRect();
+                scrollable.style.position = 'fixed';
+                scrollable.style.top = `${y}px`;
+                scrollable.style.left = '0';
+                scrollable.style.right = '0';
+                console.log(scrollable);
+            } else {
+                const scrollY = scrollable.style.top;
+                scrollable.style.position = '';
+                scrollable.style.top = '';
+                scrollable.style.left = '';
+                scrollable.style.right = '';
+                window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            }
         });
 
         return () => unsub();
