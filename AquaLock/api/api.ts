67276064a3e4b46b61449f0aca8aa181f559a4fc
shiny-mountain <shiny-mountain@lobstr.co>
2022-01-19 16:04:31 +0300

    Remove the mention of airdrop

--- AquaLock/api/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const apiUrl = 'https://airdrop-2-stats.aqua.network/';
-
-export const getSharePrice = () => {
-    return axios.get<{ share_price: string }>(apiUrl).then((res) => {
-        return res.data.share_price;
-    });
-};
