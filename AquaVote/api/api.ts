81150334f1221c8bd66734085c8b25d54a37f02c
shiny-mountain <shiny-mountain@lobstr.co>
2022-02-27 12:53:20 +0300

    Add real api

--- AquaVote/api/api.ts
+++ AquaVote/api/api.ts
@@ -16,75 +16,7 @@ const assetsListUrl = 'https://fed.stellarterm.com/issuer_orgs/';
 const assetsInfoUrl = 'https://assets.ultrastellar.com/api/v1/assets/';
 const marketKeysUrl = 'https://marketkeys-tracker.aqua.network/api/market-keys/';
 const votingTrackerUrl = 'https://voting-tracker.aqua.network/api/voting-snapshot/';
-const bribesApiUrl = 'https://bribes-api.aqua.network/api/bribes/?limit=200';
-
-const MOCK_DATA = {
-    count: 2,
-    next: null,
-    previous: null,
-    results: [
-        {
-            market_key: 'GDLJR23GLUMUTYOH2M6CMLBCLM4F7COSRCM6ZR2C43FMSC5MYW2WH44J',
-            aggregated_bribes: [
-                {
-                    market_key: 'GDLJR23GLUMUTYOH2M6CMLBCLM4F7COSRCM6ZR2C43FMSC5MYW2WH44J',
-                    total_reward_amount: '2962.5107379',
-                    start_at: '2022-02-28T00:00:00Z',
-                    stop_at: '2022-03-07T00:00:00Z',
-                    asset_code: 'XLM',
-                    asset_issuer: '',
-                    daily_amount: '423.2158197',
-                    aqua_total_reward_amount_equivalent: '144753.8876505',
-                    daily_aqua_equivalent: '20679.1268072',
-                },
-                {
-                    market_key: 'GDLJR23GLUMUTYOH2M6CMLBCLM4F7COSRCM6ZR2C43FMSC5MYW2WH44J',
-                    total_reward_amount: '665017.0000000',
-                    start_at: '2022-02-28T00:00:00Z',
-                    stop_at: '2022-03-07T00:00:00Z',
-                    asset_code: 'AQUA',
-                    asset_issuer: 'GBNZILSTVQZ4R7IKQDGHYGY2QXL5QOFJYQMXPKWRRM5PAV7Y4M67AQUA',
-                    daily_amount: '95002.4285714',
-                    aqua_total_reward_amount_equivalent: '665017.0000000',
-                    daily_aqua_equivalent: '95002.4285714',
-                },
-            ],
-        },
-        {
-            market_key: 'GBPF7NLFCYGZNHU6HS64ZGTE4YCRLAWTLFGOMFTHQ3WSUUFIGOSQFPJT',
-            aggregated_bribes: [
-                {
-                    market_key: 'GBPF7NLFCYGZNHU6HS64ZGTE4YCRLAWTLFGOMFTHQ3WSUUFIGOSQFPJT',
-                    total_reward_amount: '100000.0000000',
-                    start_at: '2022-02-28T00:00:00Z',
-                    stop_at: '2022-03-07T00:00:00Z',
-                    asset_code: 'AQUA',
-                    asset_issuer: 'GBNZILSTVQZ4R7IKQDGHYGY2QXL5QOFJYQMXPKWRRM5PAV7Y4M67AQUA',
-                    daily_amount: '14285.7142857',
-                    aqua_total_reward_amount_equivalent: '100000.0000000',
-                    daily_aqua_equivalent: '14285.7142857',
-                },
-                {
-                    market_key: 'GBPF7NLFCYGZNHU6HS64ZGTE4YCRLAWTLFGOMFTHQ3WSUUFIGOSQFPJT',
-                    total_reward_amount: '65319.2338981',
-                    start_at: '2022-02-28T00:00:00Z',
-                    stop_at: '2022-03-07T00:00:00Z',
-                    asset_code: 'DUST',
-                    asset_issuer: 'GDO6LNF7EEJS642LKPEXG5K2GQZTGADOFPJ5A34ZYHUYIRVGZMC7DUST',
-                    daily_amount: '9331.3191283',
-                    aqua_total_reward_amount_equivalent: '5879.4646133',
-                    daily_aqua_equivalent: '839.9235161',
-                },
-            ],
-        },
-    ],
-};
-
-const MOCK_AXIOS_REQUEST = (): Promise<{ data: ListResponse<MarketBribes> }> => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve({ data: MOCK_DATA }), 1000);
-    });
-};
+const bribesApiUrl = 'https://bribes-api.aqua.network/api/bribes/?limit=200&?timestamp=1646041958';
 
 export const getAssetsRequest = () => {
     return axios.get<{ issuer_orgs: any[] }>(assetsListUrl).then(({ data }) => {
@@ -173,14 +105,16 @@ export const getPairsList = async (
 
 const addKeysToMarketVotes = async (votes: MarketVotes[], count) => {
     const params = new URLSearchParams();
+    const bribesParams = new URLSearchParams();
 
     votes.forEach((marketVotes) => {
         params.append('account_id', marketVotes.market_key);
+        bribesParams.append('market_key', marketVotes.market_key);
     });
 
     const [marketsKeys, bribes] = await Promise.all([
         axios.get<ListResponse<MarketKey>>(marketKeysUrl, { params }),
-        MOCK_AXIOS_REQUEST(),
+        axios.get<ListResponse<MarketBribes>>(bribesApiUrl, { params: bribesParams }),
     ]);
 
     const pairs = votes.map((marketVotes) => {
@@ -259,7 +193,7 @@ export const getUserPairsList = async (keys: string[]) => {
         axios.get<ListResponse<MarketVotes>>(votingTrackerUrl, {
             params: marketVotesParams,
         }),
-        MOCK_AXIOS_REQUEST(),
+        axios.get<ListResponse<MarketBribes>>(bribesApiUrl, { params: marketVotesParams }),
     ]);
 
     return marketKeys.map((marketKey) => {
@@ -280,7 +214,9 @@ export const getUserPairsList = async (keys: string[]) => {
 };
 
 export const getPairsWithBribes = async (pageSize: number, page: number) => {
-    const bribes = await MOCK_AXIOS_REQUEST();
+    const bribes = await axios.get<ListResponse<MarketBribes>>(
+        `${bribesApiUrl}&limit=${pageSize}&page=${page}`,
+    );
 
     if (!bribes.data.results.length) {
         return { pairs: [], count: 0 };
