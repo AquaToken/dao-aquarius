454515616347d0be37019664ae8d510bb619250c
lush-onion <108402443+lush-onion@users.noreply.github.com>
2022-03-31 12:06:02 +0300

    Fix app name

--- common/modals/WalletConnectModals/RequestModal.tsx
+++ common/modals/WalletConnectModals/RequestModal.tsx
@@ -116,7 +116,7 @@ const RequestModal = ({ params, close }: ModalProps<RequestModalProps>) => {
                         window.open(savedApp.uri, '_blank');
                     }}
                 >
-                    Open {savedApp.name === 'Unknown' ? '' : savedApp.name}
+                    Open {name}
                 </Button>
             )}
 
