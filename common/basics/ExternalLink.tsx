6bb2221f19eabb132991a5c26e2372361c49caa2
EgorL <egor.l@razortheory.com>
2022-04-27 23:42:28 +0300

    Scroll to top after changing version

--- common/basics/ExternalLink.tsx
+++ common/basics/ExternalLink.tsx
@@ -15,15 +15,36 @@ const LinkBody = styled.a`
     white-space: nowrap;
 `;
 
+const LinkBodyDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    color: ${COLORS.purple};
+    font-size: 1.6rem;
+    line-height: 2.8rem;
+    cursor: pointer;
+    text-decoration: none;
+    white-space: nowrap;
+`;
+
 const LinkArrow = styled(LinkArrowIcon)`
     margin-left: 1rem;
 `;
 
-interface ExternalLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+interface ExternalLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement | HTMLDivElement> {
     children: string | JSX.Element;
+    asDiv?: boolean;
 }
 
-const ExternalLink = ({ children, ...props }: ExternalLinkProps): JSX.Element => {
+const ExternalLink = ({ children, asDiv, ...props }: ExternalLinkProps): JSX.Element => {
+    if (asDiv) {
+        return (
+            <LinkBodyDiv {...props}>
+                {children}
+                <LinkArrow />
+            </LinkBodyDiv>
+        );
+    }
     return (
         <LinkBody {...props} target="_blank" rel="noreferrer noopener">
             {children}
