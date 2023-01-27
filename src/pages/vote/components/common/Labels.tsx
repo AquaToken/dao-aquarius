f3a3be2156f81c1dc6b47934ea6a4e9c0531d7f3
lush-onion <108402443+lush-onion@users.noreply.github.com>
2023-01-27 13:20:12 +0300

    Fix label position

--- src/pages/vote/components/common/Labels.tsx
+++ src/pages/vote/components/common/Labels.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { useCallback, useState } from 'react';
 import Tooltip, { TOOLTIP_POSITION } from '../../../../common/basics/Tooltip';
 import styled from 'styled-components';
-import { COLORS } from '../../../../common/styles';
-import { useState } from 'react';
+import { Breakpoints, COLORS } from '../../../../common/styles';
 import { MIN_REWARDS_PERCENT } from '../MainPage/Table/Table';
+import { respondDown } from '../../../../common/mixins';
 
 const TooltipInner = styled.div`
     width: 28.8rem;
@@ -14,6 +15,10 @@ const TooltipInner = styled.div`
     a {
         margin-left: 0.5rem;
     }
+
+    ${respondDown(Breakpoints.md)`
+        width: 20rem;
+    `}
 `;
 
 const LabelWrap = styled.div`
@@ -43,65 +48,73 @@ const Label = styled.div<{ isRed?: boolean; isGreen?: boolean }>`
     cursor: help;
 `;
 
-export const BoostLabel = () => {
+const LabelComponent = ({
+    title,
+    text,
+    isGreen,
+    isRed,
+}: {
+    title: string;
+    text: string | React.ReactNode;
+    isGreen?: boolean;
+    isRed?: boolean;
+}) => {
     const [showTooltip, setShowTooltip] = useState(false);
+    const [isEnoughSpaceOnTop, setIsEnoughSpaceOnTop] = useState(true);
+
+    const ref = useCallback(
+        (node) => {
+            if (node !== null && isEnoughSpaceOnTop) {
+                setIsEnoughSpaceOnTop(node.getBoundingClientRect().left > 0);
+            }
+        },
+        [isEnoughSpaceOnTop],
+    );
+
     return (
         <Tooltip
-            content={
-                <TooltipInner
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
-                    25% boost for markets paired with AQUA, up until 5% of adjusted votes.
-                </TooltipInner>
-            }
-            position={TOOLTIP_POSITION.top}
+            content={<TooltipInner ref={ref}>{text}</TooltipInner>}
+            position={isEnoughSpaceOnTop ? TOOLTIP_POSITION.top : TOOLTIP_POSITION.right}
             isShow={showTooltip}
+            isSuccess={isGreen}
+            isError={isRed}
             onMouseEnter={() => setShowTooltip(true)}
             onMouseLeave={() => setShowTooltip(false)}
-            isSuccess
         >
             <LabelWrap>
-                <Label isGreen>boost</Label>
+                <Label isGreen={isGreen} isRed={isRed}>
+                    {title}
+                </Label>
             </LabelWrap>
         </Tooltip>
     );
 };
 
-export const RewardLabel = () => {
-    const [showTooltip, setShowTooltip] = useState(false);
-
+export const BoostLabel = () => {
     return (
-        <Tooltip
-            content={
-                <TooltipInner>
-                    Any market with at least {MIN_REWARDS_PERCENT}% of the total AQUA votes is
-                    placed into the reward zone and will get rewards after the next rewards update.
-                </TooltipInner>
-            }
-            position={TOOLTIP_POSITION.top}
-            isShow={showTooltip}
-        >
-            <Label
-                onMouseEnter={() => setShowTooltip(true)}
-                onMouseLeave={() => setShowTooltip(false)}
-            >
-                reward zone
-            </Label>
-        </Tooltip>
+        <LabelComponent
+            title={'boost'}
+            text={'25% boost for markets paired with AQUA, up until 5% of adjusted votes.'}
+            isGreen
+        />
+    );
+};
+
+export const RewardLabel = () => {
+    return (
+        <LabelComponent
+            title={'reward zone'}
+            text={`Any market with at least ${MIN_REWARDS_PERCENT}% of the total AQUA votes is placed into the reward zone and will get rewards after the next rewards update.`}
+        />
     );
 };
 
 export const AuthRequiredLabel = () => {
-    const [showTooltip, setShowTooltip] = useState(false);
-
     return (
-        <Tooltip
-            content={
-                <TooltipInner
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
+        <LabelComponent
+            title={'auth required'}
+            text={
+                <span>
                     “Authorization required” flag is enabled for one asset from the pair. With this
                     flag set, an issuer can grant a limited permissions to transact with its asset.
                     <a
@@ -111,44 +124,21 @@ export const AuthRequiredLabel = () => {
                     >
                         More details.
                     </a>
-                </TooltipInner>
+                </span>
             }
-            position={TOOLTIP_POSITION.top}
-            isShow={showTooltip}
-            isError
-            onMouseEnter={() => setShowTooltip(true)}
-            onMouseLeave={() => setShowTooltip(false)}
-        >
-            <LabelWrap>
-                <Label isRed>auth required</Label>
-            </LabelWrap>
-        </Tooltip>
+            isRed
+        />
     );
 };
 
 export const NoLiquidityLabel = () => {
-    const [showTooltip, setShowTooltip] = useState(false);
-
     return (
-        <Tooltip
-            content={
-                <TooltipInner
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
-                    This market pair is not eligible for AQUA rewards at the moment, as it failed
-                    the liquidity test (no path payment from XLM).
-                </TooltipInner>
+        <LabelComponent
+            title={'no liquidity'}
+            text={
+                'This market pair is not eligible for AQUA rewards at the moment, as it failed the liquidity test (no path payment from XLM).'
             }
-            position={TOOLTIP_POSITION.top}
-            isShow={showTooltip}
-            isError
-            onMouseEnter={() => setShowTooltip(true)}
-            onMouseLeave={() => setShowTooltip(false)}
-        >
-            <LabelWrap>
-                <Label isRed>no liquidity</Label>
-            </LabelWrap>
-        </Tooltip>
+            isRed
+        />
     );
 };
