860ade0a7c38b65ea7754d76b7de30145cbd0f13
lush-onion <108402443+lush-onion@users.noreply.github.com>
2023-07-25 14:00:16 +0200

    Update roadmap

--- src/pages/main/components/Roadmap/constants.ts
+++ src/pages/main/components/Roadmap/constants.ts
@@ -1,4 +1,4 @@
-export const completedStepsCount = 4;
+export const completedStepsCount = 9;
 export const steps = [
     {
         date: 'Q4 - 2020',
@@ -18,11 +18,56 @@ export const steps = [
     {
         date: 'Q4 - 2021',
         title: 'Aquarius v0.2: Voting',
-        text: 'New set of updates will allow AQUA holders to control how Aquarius works. They will drive liquidity to specific market pairs and vote for assets to be grouped in default lists. We will work on introducing AQUA tokens in various products built on Stellar.',
+        text: 'This set of updates allowed AQUA holders to control how Aquarius works. Governance creates a way for holders to decide which direction to take Aquarius with governance proposals, and the ability to vote for markets they wish to see receive liquidity rewards was enabled.',
+    },
+    {
+        date: 'Q1 - 2022',
+        title: 'Airdrop 2 & locker tool',
+        text: 'Airdrop 2 saw 15 billion AQUA split amongst those who held over 500 XLM + 1 AQUA  at the time of the snapshot, with eligible users able to claim rewards monthly until Feb 2025. The introduction of the locker tool allowed the ability for users to boost their airdrop rewards.',
     },
     {
         date: 'Q2 - 2022',
-        title: 'Aquarius v1.0: DAO',
-        text: 'The end goal is to build a thriving, reliable and decentralized ecosystem around Aquarius and AQUA tokens. Key protocol parameters will be controlled by the AQUA token holders. This will be further explored as the smart contract frameworks on Stellar mature and evolve.',
+        title: 'Aquarius v0.3: Bribes & gov v2',
+        text: 'Bribes introduce a way for users to advertise their assets & markets to the Aquarius community by allowing incentives to be placed on markets, encouraging users to vote for specific markets. Governance v2 also launched, bringing a new discussion phase and fee structure.',
+    },
+    {
+        date: 'Q3 - 2022',
+        title: 'Aquarius v0.4: ICE',
+        text: 'ICE brings a new dynamic to Aquarius! The airdrop 2 locker tool got upgraded, allowing users to freeze their AQUA into 4 ICE tokens. Each ICE token has its benefits, including allowing users to vote for liquidity rewards & governance simultaneously and boosting their SDEX & AMM rewards.',
+    },
+    {
+        date: 'Q4 - 2022',
+        title: 'Smart contract research',
+        text: 'Stellar’s smart contracts are close on the horizon. We have already started researching how Soroban will function, and in 2023 we will be focusing on discovering how smart contracts can benefit the Aquarius ecosystem.',
+    },
+    {
+        date: 'Q1 - 2023',
+        title: 'Aquarius Account Dashboard',
+        text: 'A major upgrade for aqua.network! Users will be able to access all aspects of their interactions with Aquarius, with all votes, rewards earned, and other stats available in one simple dashboard interface.',
+    },
+    {
+        date: 'Q2 - 2023',
+        title: 'Aquarius v0.5: Upgrading AQUA Rewards',
+        text: 'This upgrade intends to balance the distribution of daily liquidity rewards on a market-by-market basis. Markets that mainly see SDEX volume will see increased SDEX rewards, whereas markets that rely on AMMs will see higher AMM rewards.',
+    },
+    {
+        date: 'Q3 - 2023',
+        title: 'SCF 15 - Soroban & Aquarius',
+        text: 'Using funding via the Stellar Community Fund, we are building new systems for Aquarius using Soroban smart contracts. First will be Aquarius AMMs, introducing new AMM types to Stellar in 2024.',
+    },
+    {
+        date: 'Q4 - 2023',
+        title: 'Launch of Aquarius AMMs',
+        text: 'The deployment of Aquarius AMMs will aim to coincide with the launch of Soroban, expected around the Meridian conference this year (September 2023).',
+    },
+    {
+        date: 'Q2 - 2024',
+        title: 'Decentralization of reward engines',
+        text: 'Continued exploration of Soroban smart contracts will see a more decentralized Aquarius emerge. We plan to move the rewards engines over to Soroban, allowing hourly AQUA rewards to distribute via smart contracts.',
+    },
+    {
+        date: 'Q4 - 2024',
+        title: 'Improvements to AMMs and engines',
+        text: 'As Soroban continues to mature, we will improve upon our AMMs and reward distribution engines. New features can also be explored, and further movements toward a fully operational DAO can be achieved.',
     },
 ];
