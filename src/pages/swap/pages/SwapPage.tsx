57b25857c8ab45a6776f57a801d5018c99058f26
lush-onion <e@aqua.network>
2024-06-27 11:08:10 +0200

    Increase padding

--- src/pages/swap/pages/SwapPage.tsx
+++ src/pages/swap/pages/SwapPage.tsx
@@ -46,7 +46,7 @@ const Container = styled.main`
     flex-direction: column;
     scroll-behavior: smooth;
     overflow: auto;
-    padding-bottom: 6rem;
+    padding-bottom: 7rem;
 `;
 
 const Content = styled.div`
