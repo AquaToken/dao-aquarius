2b60db5ce791fc7176d6864743a7a33881a988e8
lush-onion <e@aqua.network>
2024-08-16 13:14:13 +0200

    Fix broken requests for asset and price details when accounts have too many trustlines

--- src/store/assetsStore/api/api.ts
+++ src/store/assetsStore/api/api.ts
@@ -21,14 +21,30 @@ export const getAssetsRequest = () => {
     });
 };
 
-export const getAssetsInfo = (assets) => {
-    const params = new URLSearchParams();
+export const getAssetsInfo = async (
+    assets: Array<{ code: string; issuer: string }>,
+    batchSize: number = 100,
+): Promise<Asset[]> => {
+    // Function to fetch a batch of assets
+    const fetchBatch = async (batch: Array<{ code: string; issuer: string }>) => {
+        const params = new URLSearchParams();
+        batch.forEach((asset) => params.append('asset', `${asset.code}:${asset.issuer}`));
 
-    assets.forEach((asset) => {
-        params.append('asset', `${asset.code}:${asset.issuer}`);
-    });
+        const { data } = await axios.get<ListResponse<Asset>>(assetsInfoUrl, { params });
+        return data.results;
+    };
 
-    return axios
-        .get<ListResponse<Asset[]>>(assetsInfoUrl, { params })
-        .then(({ data }) => data.results);
+    // Split assets into batches and fetch all batches concurrently
+    const results = await Promise.all(
+        assets
+            .reduce((batches, _, i) => {
+                if (i % batchSize === 0) batches.push([]);
+                batches[batches.length - 1].push(assets[i]);
+                return batches;
+            }, [] as Array<Array<{ code: string; issuer: string }>>)
+            .map(fetchBatch),
+    );
+
+    // Combine the results into a single array
+    return results.flat();
 };
