0d497fab804cb88bc06825a920f568e5944c7fc8
lush-onion <e@aqua.network>
2024-09-30 18:11:14 +0200

    fix: staging API url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,6 +2,6 @@ export const API_LIMIT = 200;
 
 export const API_URL_STELLAR_EXPERT = 'https://api.stellar.expert/';
 export const API_URL_ICE_LOCKER = 'https://ice-distributor.aqua.network/api';
-export const API_AMM_BACKEND = 'https://amm-api.aqua.network';
+export const API_AMM_BACKEND = 'https://amm-api-staging.aqua.network';
 export const API_REWARDS_BACKEND = 'https://reward-api.aqua.network/api/rewards/';
 export const API_CMC = 'https://cmc.aqua.network/api';
