7134da3096918363e930063bbc8ba2dc9c58458d
dry-cake <174254844+dry-cake@users.noreply.github.com>
2024-10-29 20:12:05 +0100

    fix: empty LS env crash

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,9 +1,8 @@
-import { API_URLS } from 'constants/api';
 import { ENV_PRODUCTION, ENV_TESTNET } from 'constants/env';
 import { LS_ENV_NAME } from 'constants/local-storage';
 import { NETWORK_PASSPHRASES } from 'constants/stellar';
 
-export const getEnv = () => localStorage.getItem(LS_ENV_NAME);
+export const getEnv = () => localStorage.getItem(LS_ENV_NAME) || ENV_PRODUCTION;
 export const getIsTestnetEnv = () => getEnv() === ENV_TESTNET;
 export const getIsProductionEnv = () => getEnv() === ENV_PRODUCTION;
 export const setTestnetEnv = () => {
