49241eae470b0354f7f494ac35e5fa57ca276e10
lush-onion <e@aqua.network>
2024-12-20 11:03:34 +0100

    fix: staging api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -9,15 +9,13 @@ export const API_CMC = 'https://cmc.aqua.network/api';
 export const API_ASSETS_LIST_URL = 'https://fed.stellarterm.com/issuer_orgs/';
 export const API_ASSETS_INFO = 'https://assets.aqua.network/api/v1/assets/';
 
-export const API_AMM_BACKEND_STAGING = 'https://amm-api-staging.aqua.network';
-
 export const API_URLS = {
     [ENV_PRODUCTION]: {
         moonpay: 'https://api.moonpay.com/v3',
         onRampProxy: 'https://soroban-on-ramp.coindisco.com',
         horizon: 'https://horizon.stellar.org',
         soroban: 'https://soroban-rpc.aqua.network/',
-        ammAqua: 'https://amm-api.aqua.network',
+        ammAqua: 'https://amm-api-staging.aqua.network', //staging
     },
     [ENV_TESTNET]: {
         moonpay: 'https://api.moonpay.com/v3',
