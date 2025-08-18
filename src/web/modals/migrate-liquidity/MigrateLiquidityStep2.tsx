090244f4cbca48c95c96387af0efd7955d4c5dd8
dry-cake <174254844+dry-cake@users.noreply.github.com>
2025-08-18 15:41:08 +0400

    fix: liq migrate scroll

--- src/web/modals/migrate-liquidity/MigrateLiquidityStep2.tsx
+++ src/web/modals/migrate-liquidity/MigrateLiquidityStep2.tsx
@@ -10,19 +10,6 @@ import PoolsList from 'pages/amm/components/PoolsList/PoolsList';
 
 import { Stepper } from './MigrateLiquidityStep1';
 
-import { customScroll, respondDown } from '../../mixins';
-import { Breakpoints } from '../../styles';
-
-const Content = styled.div`
-    max-height: 60vh;
-    ${customScroll};
-    overflow-y: auto;
-    padding-top: 8rem;
-
-    ${respondDown(Breakpoints.md)`
-        max-height: unset;
-    `}
-`;
 interface MigrateLiquidityStep2Params {
     poolsToMigrate: PoolProcessed[];
     baseAmount: string;
@@ -39,18 +26,17 @@ const MigrateLiquidityStep2 = ({ params, confirm }: ModalProps<MigrateLiquidityS
         <ModalWrapper $isWide>
             <Stepper>STEP 2/2</Stepper>
             <ModalTitle>Deposit to Soroban pool</ModalTitle>
-            <Content>
-                <PoolsList
-                    pools={poolsToMigrate}
-                    withDeposit
-                    baseAmount={baseAmount}
-                    counterAmount={counterAmount}
-                    base={base}
-                    counter={counter}
-                    onUpdate={() => onUpdate()}
-                    onConfirm={() => confirm()}
-                />
-            </Content>
+
+            <PoolsList
+                pools={poolsToMigrate}
+                withDeposit
+                baseAmount={baseAmount}
+                counterAmount={counterAmount}
+                base={base}
+                counter={counter}
+                onUpdate={() => onUpdate()}
+                onConfirm={() => confirm()}
+            />
         </ModalWrapper>
     );
 };
