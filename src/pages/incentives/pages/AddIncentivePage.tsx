af4fa58c168106ac85481c81785d72bf7310faa4
lush-onion <108402443+lush-onion@users.noreply.github.com>
2026-03-23 10:52:32 +0100

    refactor: reuse fallback back navigation

--- src/pages/incentives/pages/AddIncentivePage.tsx
+++ src/pages/incentives/pages/AddIncentivePage.tsx
@@ -1,6 +1,7 @@
 import { addDays, startOfDay } from 'date-fns';
 import * as React from 'react';
 import { useEffect, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { findSwapPath, getAssetsList, getPoolsForIncentives } from 'api/amm';
@@ -17,6 +18,7 @@ import { contractValueToAmount, contractValueToFormattedAmount } from 'helpers/a
 import { getAquaAssetData } from 'helpers/assets';
 import { convertUTCToLocalDateIgnoringTimezone } from 'helpers/date';
 import { formatBalance } from 'helpers/format-number';
+import { navigateBackWithFallback } from 'helpers/navigation';
 import { getTokensFromCache } from 'helpers/token';
 
 import { useDebounce } from 'hooks/useDebounce';
@@ -154,6 +156,7 @@ enum Step {
 }
 
 const AddIncentivePage = () => {
+    const navigate = useNavigate();
     const [markets, setMarkets] = useState<PoolProcessed[] | null>(null);
     const [selectedMarket, setSelectedMarket] = useState<string | null>(null);
     const [step, setStep] = useState(Step.market);
@@ -384,7 +387,12 @@ const AddIncentivePage = () => {
                 <FormPageContentWrap>
                     <FormBackButton
                         label="Pool Incentives"
-                        to={AppRoutes.section.incentive.link.index}
+                        onClick={() =>
+                            navigateBackWithFallback(
+                                navigate,
+                                AppRoutes.section.incentive.link.index,
+                            )
+                        }
                     >
                         <ArrowLeft />
                     </FormBackButton>
